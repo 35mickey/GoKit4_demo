e5cc950c604a9c8ed81b60a99685ef1120920b02
35mickey <35mickey@163.com>
2019-12-28 22:34:25 +0800

    去除了以前自己手动控制版本时的历史工程

--- Backup/2018.11.14/GoKit4_Demo/IF_ERROR.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal enabledelayedexpansion
-
-set PRJ_PATH=%~dp0
-set PROJECT_PATH=%PRJ_PATH:~,-1%
-
-cd %PROJECT_PATH%
-
-@cmd.exe
--- Backup/2018.11.9/GoKit4_Demo/IF_ERROR.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd\
-G:
-cd "TEMP\GoKit4_Demo"
-@cmd.exe
